110ba547020aa891672e2d757481ea08f7d1df40
Emma Burton <emma.burton@bbc.co.uk>
2019-04-22 19:33:17 +0100

    Add recyclerview dependency

--- app/src/main/java/com/example/shoppinglist/list/ShoppingListModel.kt
+++ app/src/main/java/com/example/shoppinglist/list/ShoppingListModel.kt
@@ -1,6 +1,23 @@
 package com.example.shoppinglist.list
 
-class ShoppingListModel {
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.example.shoppinglist.data.Item
+
+class ShoppingListModel(
+    private val shoppingList: List<Item>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>()  {
+
+
+    override fun getItemCount(): Int = shoppingList.size
+
+    override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 
 }
