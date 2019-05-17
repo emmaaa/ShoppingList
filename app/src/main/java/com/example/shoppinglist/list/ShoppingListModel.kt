ea542a0a7e7a76d246c916c621ae6b5ec5f20f80
Emma Burton <emma.burton@bbc.co.uk>
2019-05-17 10:59:48 +0100

    Add ShoppingListModel and rename MainActivity to ShoppingListActivity

--- app/src/main/java/com/example/shoppinglist/list/ShoppingListModel.kt
+++ app/src/main/java/com/example/shoppinglist/list/ShoppingListModel.kt
@@ -1,23 +1,20 @@
 package com.example.shoppinglist.list
 
-import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.ViewGroup
 import com.example.shoppinglist.data.ShoppingListItem
 
-class ShoppingListModel(
-    private val shoppingList: List<ShoppingListItem>
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>()  {
+class ShoppingListModel(private val shoppingList: List<ShoppingListItem>) {
 
 
-    override fun getItemCount(): Int = shoppingList.size
+    fun getItemCount(): Int = shoppingList.size
 
-    override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+    fun onCreateViewHolder(parent: ViewGroup, viewType: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView // todo bind view
     }
-
 
 }
