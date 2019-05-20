d5ceac37704555c0a05c4a88901724e414146867
Emma Burton <emma.burton@bbc.co.uk>
2019-05-20 10:25:40 +0100

    Add login screen

--- app/src/main/java/com/example/shoppingList/MainActivity.kt
+++ app/src/main/java/com/example/shoppingList/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.shoppingList
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class MainActivity : AppCompatActivity() {
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.main_activity)
-        }
-    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
 
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+         val googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+}
